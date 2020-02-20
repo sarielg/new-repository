4fd5c948c2f5baf449c3daaec543a07f2c8da5e1
שריאל גבריאלי (LSARIELG) <301313102@gov.il>
2020-02-20 08:33:04 +0200

    excrise5 #!

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
-ssh-keygen
-ssh-copy-id -i ~/.ssh/id_rsa.pub server2
-ssh server2
+#ssh-keygen
+#ssh-copy-id -i ~/.ssh/id_rsa.pub server2
+#ssh server2
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
-ssh-keygen
-ssh-copy-id -i ~/.ssh/id_rsa.pub server1
-ssh server1
+#ssh-keygen
+#ssh-copy-id -i ~/.ssh/id_rsa.pub server1
+#ssh server1
