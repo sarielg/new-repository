59a5c229e9a572fc430b7bf1732ebe9054a66e3a
שריאל גבריאלי (LSARIELG) <301313102@gov.il>
2020-02-24 11:04:59 +0200

    excrise5 done!

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+apt-get install sshpass
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
 cat ~/.ssh//id_rsa.pub | sshpass -p 'vagrant' ssh vagrant@server2 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
