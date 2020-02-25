b9e610a507f0617cade7f814870edf9e406a85aa
שריאל גבריאלי (LSARIELG) <301313102@gov.il>
2020-02-25 14:50:02 +0200

    excrise6 done!

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+#echo ${@: -1}
+#echo $#
+target_server=server1
+files=( "$@" )
+len=${#files[@]}
+target_path=${files[$(( $len - 1 ))]}
+total_size=0
+for (( i=0; i<len-1; i++ ))
+do
+        #echo ${files[$(( $len - 1 ))]}
+        echo $target_path
+        source_file=${files[$i]}
+        echo $source_file
+        #size=$(/usr/bin/wc -c $source_file)
+        #echo $size
+        file_size=$(($(sudo wc -c < "$source_file")))
+        #echo $size
+        #size=$(($(wc -c < "$source_file") +7))
+        #echo $file_size
+        total_size=$(( total_size + file_size))
+        sudo scp $source_file root@$target_server:$target_path
+done
+echo $total_size
